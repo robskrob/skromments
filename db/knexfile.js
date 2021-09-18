3f3cce4d7c57d6fb0801e14e4c5dadf222934821
Rob Jewell <jewell.robertp@gmail.com>
2021-09-18 13:51:32 -0400

    WIP: setting up DB

---
+++ db/knexfile.js
@@ -0,0 +1,17 @@
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'development_skromments',
+      user: 'robskrob',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+}
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const express = require('exprss')
+
+const router = express.Router()
+
+module.exports = router
