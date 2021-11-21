b1729416f1a4fc3c5a274c4486143e0726ee3d3c
Rob Jewell <jewell.robertp@gmail.com>
2021-11-21 15:16:56 -0500

    use outdirectory for tsc build

--- api/db/migrations/20210918175327_init.ts
+++ api/db/migrations/20210918175327_init.ts
@@ -1,11 +1,11 @@
-exports.up = function (knex) {
-  return knex.schema.createTable('comments', table => {
+exports.up = function (knex: any) {
+  return knex.schema.createTable('comments', (table: any) => {
     table.increments()
     table.string('content')
     table.timestamps(true, true)
   })
 }
 
-exports.down = function (knex) {
+exports.down = function (knex: any) {
   return knex.schema.dropTable('comments')
 }
